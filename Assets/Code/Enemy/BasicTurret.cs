36b60a27a245c5ca054d112f02b73de904630177
Aescosaurus <michaelbuslik@hotmail.com>
2022-07-02 16:20:23 -0600

    Basic turrets & enemy folder organization.

---
+++ Assets/Code/Enemy/BasicTurret.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTurret
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		gunSpot = transform.Find( "GunSpot" );
+
+		if( startRandomized ) refire.Randomize();
+	}
+
+	void Update()
+	{
+		if( refire.Update( Time.deltaTime ) )
+		{
+			FireBullet();
+
+			refire.Reset();
+		}
+	}
+
+	void FireBullet()
+	{
+		var bullet = Instantiate( bulletPrefab,gunSpot.position,Quaternion.identity );
+
+		bullet.GetComponent<Rigidbody2D>().AddForce( -transform.right * shotSpd,ForceMode2D.Impulse );
+
+		Destroy( bullet,5.0f );
+	}
+
+	Transform gunSpot;
+
+	[SerializeField] Timer refire = new Timer( 2.0f );
+	[SerializeField] GameObject bulletPrefab = null;
+	[SerializeField] float shotSpd = 1.0f;
+	[SerializeField] bool startRandomized = true;
+}
--- Assets/Code/Util/Timer.cs
+++ Assets/Code/Util/Timer.cs
@@ -22,6 +22,11 @@ public class Timer
 	{
 		curTime = 0.0f;
 	}
+	
+	public void Randomize()
+	{
+		curTime = Random.Range( 0.0f,maxTime );
+	}
 
 	public bool IsDone()
 	{
