26ecf21770575e09c160d34d9412ccbc94ee7aa9
Aescosaurus <michaelbuslik@hotmail.com>
2022-07-07 06:43:16 -0600

    Patrol fox & patrol manager working.

--- Assets/Code/Enemy/PatrolFox.cs
+++ Assets/Code/Enemy/PatrolFox.cs
@@ -6,4 +6,69 @@ public class PatrolFox
 	:
 	MonoBehaviour
 {
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+
+		StartCoroutine( WaitRegenPath() );
+	}
+
+	void Update()
+	{
+		if( curPoint > -1 )
+		{
+			var diff = curPath[curPoint] - ( Vector2 )transform.position;
+
+			if( diff.sqrMagnitude < stopDist * stopDist ) TargetNextPoint();
+
+			if( curPoint > -1 ) body.velocity = diff.normalized * moveSpd;
+			else body.velocity = Vector2.zero;
+		}
+	}
+
+	IEnumerator WaitRegenPath()
+	{
+		yield return( new WaitForSeconds( activateDelay ) );
+		RegenPath();
+	}
+
+	void RegenPath()
+	{
+		curPath = PatrolManager.Get().GeneratePatrolPath( transform.position );
+		curPoint = 0;
+	}
+
+	void TargetNextPoint()
+	{
+		if( curPath.Count == 1 ) curPoint = -1;
+		else
+		{
+			curPoint += pathDir;
+			if( curPoint >= curPath.Count )
+			{
+				pathDir = -1;
+				curPoint = curPath.Count - 1;
+			}
+			else if( curPoint < 0 )
+			{
+				pathDir = 1;
+				curPoint = 0;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D( Collision2D coll )
+	{
+		// RegenPath();
+	}
+
+	Rigidbody2D body;
+
+	List<Vector2> curPath;
+	int curPoint = -1;
+	int pathDir = 1;
+
+	[SerializeField] float activateDelay = 0.1f;
+	[SerializeField] float moveSpd = 1.0f;
+	[SerializeField] float stopDist = 0.4f;
 }
---
+++ Assets/Code/Enemy/PatrolManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PatrolManager
+	:
+	MonoBehaviour
+{
+	class PatrolPoint
+	{
+		public PatrolPoint( GameObject point )
+		{
+			this.point = point;
+		}
+
+		public GameObject point = null;
+		public List<PatrolPoint> connected = new List<PatrolPoint>();
+	}
+
+	public static PatrolManager Get()
+	{
+		Assert.IsNotNull( self );
+		return( self );
+	}
+
+	void Start()
+	{
+		self = this;
+
+		patrolMask = LayerMask.GetMask( "Default" );
+
+		var patrolPoints = GameObject.FindGameObjectsWithTag( "Patrol" );
+		foreach( var point in patrolPoints )
+		{
+			points.Add( new PatrolPoint( point ) );
+		}
+
+		for( int i = 0; i < points.Count; ++i )
+		{
+			var curPoint = points[i];
+			for( int j = i; j < points.Count; ++j )
+			{
+				var testPoint = points[j];
+				if( CheckConnected( curPoint.point.transform.position,testPoint.point.transform.position ) )
+				{
+					curPoint.connected.Add( testPoint );
+					testPoint.connected.Add( curPoint );
+				}
+			}
+		}
+	}
+
+	// if point is within patrol range & has line of sight
+	bool CheckConnected( Vector2 p1,Vector2 p2 )
+	{
+		var diff = p2 - p1;
+		if( diff.sqrMagnitude > patrolRange * patrolRange ) return( false );
+
+		var hit = Physics2D.Raycast( p1,diff,patrolRange,patrolMask );
+		
+		return( !hit );
+	}
+
+	// Generate path based on cur pos
+	public List<Vector2> GeneratePatrolPath( Vector2 pos )
+	{
+		var nearest = new List<PatrolPoint>();
+		foreach( var point in points )
+		{
+			if( CheckConnected( pos,point.point.transform.position ) ) nearest.Add( point );
+		}
+
+		PatrolPoint closest = null;
+		float closestDist = 999999.0f;
+		for( int i = 0; i < nearest.Count; ++i )
+		{
+			var dist = ( ( Vector2 )nearest[i].point.transform.position - pos ).sqrMagnitude;
+			if( dist < closestDist )
+			{
+				closestDist = dist;
+				closest = nearest[i];
+			}
+		}
+
+		if( closest != null )
+		{
+			var patrolPath = new List<PatrolPoint>();
+			patrolPath.Add( closest );
+			for( int i = 0; i < maxPathSize; ++i )
+			{
+				foreach( var neigh in patrolPath[patrolPath.Count - 1].connected )
+				{
+					if( !patrolPath.Contains( neigh ) ) patrolPath.Add( neigh );
+				}
+			}
+
+			var vec2Path = new List<Vector2>();
+			foreach( var point in patrolPath ) vec2Path.Add( point.point.transform.position );
+			return( vec2Path );
+		}
+		else return( null );
+	}
+
+	static PatrolManager self = null;
+
+	LayerMask patrolMask;
+	List<PatrolPoint> points = new List<PatrolPoint>();
+
+	[SerializeField] float patrolRange = 20.0f;
+	[SerializeField] float maxPathSize = 4;
+}
