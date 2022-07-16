dd14c506ee178d4659116b0d0e4b1acd5ebd6bf0
Aescosaurus <michaelbuslik@hotmail.com>
2022-07-16 12:45:10 -0600

    Key follows player when picked up.

---
+++ Assets/Code/Key.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key
+	:
+	MonoBehaviour
+{
+	void Update()
+	{
+		if( following )
+		{
+			if( ( followTarget.transform.position - transform.position ).sqrMagnitude > moveThresh * moveThresh )
+			{
+				transform.position = Vector2.Lerp( transform.position,followTarget.transform.position,
+					Mathf.Min( 1.0f,lerpAmount * Time.deltaTime ) );
+			}
+		}
+	}
+
+	void OnTriggerEnter2D( Collider2D coll )
+	{
+		if( following ) return;
+
+		if( coll.tag == "Player" )
+		{
+			following = true;
+			followTarget = coll.gameObject;
+		}
+	}
+
+	bool following = false;
+	GameObject followTarget = null;
+
+	[SerializeField] float moveThresh = 1.2f;
+	[SerializeField] float lerpAmount = 0.3f;
+}
