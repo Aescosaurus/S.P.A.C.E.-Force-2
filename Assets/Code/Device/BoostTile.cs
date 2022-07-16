4b305650ed51739d2a2ac933618cca2894f76f22
Aescosaurus <michaelbuslik@hotmail.com>
2022-07-16 17:59:06 -0600

    Boost tiles!

---
+++ Assets/Code/Device/BoostTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostTile
+	:
+	MonoBehaviour
+{
+	void OnTriggerEnter2D( Collider2D coll )
+	{
+		if( coll.tag == "Player" )
+		{
+			var playerBody = coll.GetComponent<Rigidbody2D>();
+			playerBody.velocity *= velTamper;
+			playerBody.AddForce( transform.up * pushForce,ForceMode2D.Impulse );
+			// todo: particle trail and/or actual trail on player
+		}
+	}
+
+	[SerializeField] float pushForce = 2.0f;
+	[SerializeField] float velTamper = 0.7f;
+}
