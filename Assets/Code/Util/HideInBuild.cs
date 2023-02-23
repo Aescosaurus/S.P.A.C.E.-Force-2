27246572b6e51c9e870a1697e317d4a05eb3fcc4
Aescosaurus <michaelbuslik@hotmail.com>
2023-02-22 21:20:32 -0700

    HideInBuild script to disable debug sprites.

---
+++ Assets/Code/Util/HideInBuild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideInBuild
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		if( !Application.isEditor )
+		{
+			GetComponentInChildren<SpriteRenderer>().enabled = false;
+		}
+	}
+}
