de0799b8d08e602b41afd28d2c080b816293deec
aescosaurus <michaelbuslik@hotmail.com>
2024-09-06 21:37:22 -0600

    Level 2 start.

---
+++ Assets/Code/Device/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner
+	:
+	MonoBehaviour
+{
+	void Update()
+	{
+		transform.Rotate( 0.0f,0.0f,spinSpd * Time.deltaTime );
+	}
+
+	[SerializeField] float spinSpd = 30.0f;
+}
