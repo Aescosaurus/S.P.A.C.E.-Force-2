6eb02ec57512751808b94b682c731f088d8890be
aescosaurus <michaelbuslik@hotmail.com>
2024-09-07 16:03:07 -0600

    Crystal mechanic: hit crystal changer to de/activate crystal shards.

---
+++ Assets/Code/Device/CrystalChanger.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CrystalChanger
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+#if UNITY_EDITOR && false
+		int nEnabled = 0;
+		foreach( var color in colorEnabled )
+		{
+			if( color ) ++nEnabled;
+		}
+		Assert.IsTrue( nEnabled >= 2 );
+
+		Assert.IsTrue( colorEnabled[( int )startColor] );
+#endif
+		sprRend = GetComponentInChildren<SpriteRenderer>();
+
+		hp = hitsToActivate;
+
+		var crystals = GameObject.FindGameObjectsWithTag( "Crystal" );
+		foreach( var crystal in crystals )
+		{
+			var shard = crystal.GetComponent<CrystalShard>();
+			if( shard.crystalId == crystalId ) myCrystals.Add( shard );
+		}
+		
+		curColor = startColor;
+		if( !colorEnabled[( int )startColor] ) NextColor();
+		else SetColor();
+	}
+
+	void OnCollisionEnter2D( Collision2D coll )
+	{
+		if( coll.gameObject.tag == "Bullet" )
+		{
+			if( --hp <= 0 )
+			{
+				NextColor();
+
+				hp = hitsToActivate;
+			}
+		}
+	}
+
+	void NextColor()
+	{
+		int nextCol = ( int )curColor + 1;
+		int resets = 2; // protection against infinite loop
+		while( !colorEnabled[nextCol] )
+		{
+			++nextCol;
+			if( nextCol >= ( int )CrystalShard.ShardColor.Count )
+			{
+				nextCol = 0;
+				if( --resets <= 0 ) break;
+			}
+		}
+		curColor = ( CrystalShard.ShardColor )nextCol;
+		SetColor();
+	}
+
+	void SetColor()
+	{
+		foreach( var crystal in myCrystals ) crystal.OnColorChange( curColor );
+
+		sprRend.sprite = crystalSprs[( int )curColor];
+	}
+
+	SpriteRenderer sprRend;
+
+	List<CrystalShard> myCrystals = new List<CrystalShard>();
+
+	[SerializeField] int hitsToActivate = 5;
+	int hp;
+	
+	[SerializeField] List<Sprite> crystalSprs = new List<Sprite>();
+
+	// crystalId used to track which crystals this changer can activate
+	[SerializeField] int crystalId = 1;
+	[SerializeField] List<bool> colorEnabled = new List<bool>();
+
+	[SerializeField] CrystalShard.ShardColor startColor = CrystalShard.ShardColor.Blue;
+	CrystalShard.ShardColor curColor;
+}
---
+++ Assets/Code/Device/CrystalShard.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CrystalShard
+	:
+	MonoBehaviour
+{
+	public enum ShardColor
+	{
+		Blue = 0,
+		Green = 1,
+		Yellow = 2,
+		Count
+	}
+
+	void Awake()
+	{
+		Assert.IsTrue( myColor != ShardColor.Count );
+
+		sprRend = GetComponentInChildren<SpriteRenderer>();
+		sprRend.sprite = shardSprs[( int )myColor];
+		coll = GetComponent<Collider2D>();
+	}
+
+	public void OnColorChange( ShardColor newColor )
+	{
+		bool colorMatch = ( newColor == myColor );
+		
+		coll.enabled = !colorMatch;
+		sprRend.sprite = ( colorMatch ? hollowSprs : shardSprs )[( int )myColor];
+	}
+
+	SpriteRenderer sprRend;
+	Collider2D coll;
+
+	[SerializeField] List<Sprite> shardSprs = new List<Sprite>();
+	[SerializeField] List<Sprite> hollowSprs = new List<Sprite>();
+	[SerializeField] public int crystalId = 1;
+	[SerializeField] ShardColor myColor = ShardColor.Blue;
+}
